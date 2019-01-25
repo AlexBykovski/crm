2be11509a0029c35dda218d72952a88057f78838
AlexBykovski <bykovski.work@gmail.com>
2019-01-25 08:49:08 +0300

    add tasks

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -26,13 +28,44 @@ abstract class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var Collection
+     *
+     * One product has many features. This is the inverse side.
+     * @ORM\OneToMany(targetEntity="Task", mappedBy="user")
+     */
+    protected $tasks;
+
     public function __construct()
     {
         parent::__construct();
+
+        $this->tasks = new ArrayCollection();
     }
 
     public function getSearchStatuses(){}
 
     public function getRole(){}
+
+    /**
+     * @return Collection
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    /**
+     * @param Collection $tasks
+     */
+    public function setTasks(Collection $tasks): void
+    {
+        $this->tasks = $tasks;
+    }
+
+    public function addTask(Task $task)
+    {
+        $this->tasks->add($task);
+    }
 }
 
