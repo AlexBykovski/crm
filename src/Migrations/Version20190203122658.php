548b053573b30487e5aad278367c338a81707489
AlexBykovski <bykovski.work@gmail.com>
2019-02-03 15:32:11 +0300

    change migration

--- src/Migrations/Version20190203122658.php
+++ src/Migrations/Version20190203122658.php
@@ -15,7 +15,7 @@ final class Version20190203122658 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE manager ADD work_updated_at DATETIME  DEFAULT NULL');
+        $this->addSql('ALTER TABLE manager ADD work_updated_at DATETIME DEFAULT NULL');
 
         $this->addSql('UPDATE manager SET work_updated_at = "2019-02-02 00:00:00"');
     }
